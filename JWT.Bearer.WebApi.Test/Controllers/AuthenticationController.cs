c659d9127ddb1f4c4251f8f70aca5dd5088895fb
wangdong <597365277@qq.com>
2019-10-25 15:40:34 +0800

    格式化代码

--- JWT.Bearer.WebApi.Test/Controllers/AuthenticationController.cs
+++ JWT.Bearer.WebApi.Test/Controllers/AuthenticationController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace JWT.Bearer.WebApi.Test.Controllers
 {
