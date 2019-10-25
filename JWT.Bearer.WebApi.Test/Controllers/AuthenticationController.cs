643f5d3603762964d5ed77133b1083d4cc7889cf
wangdong <597365277@qq.com>
2019-10-25 12:01:29 +0800

    添加项目文件。

---
+++ JWT.Bearer.WebApi.Test/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.Bearer.WebApi.Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private  IConfiguration _configuration { get; }
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Post()
+        {
+            var authenticationHeader = Request.Headers["Authorization"].First();
+            var key = authenticationHeader.Split(' ')[1];
+            var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(key)).Split(':');
+            var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:ServerSecret"]));
+            if (credentials[0] == "username" && credentials[1] == "password")
+            {
+                var result = new {token = GenerateToken(serverSecret)};
+                return Ok(result);
+            }
+
+            return BadRequest();
+        }
+
+        private string GenerateToken(SymmetricSecurityKey key)
+        {
+            var now = DateTime.UtcNow;
+            var issuer = _configuration["JWT:Issuer"];
+            var audience = _configuration["JWT:Audience"];
+            var identity = new ClaimsIdentity();
+            var signingCredentials=new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateJwtSecurityToken(issuer, audience, identity, now,
+                now.Add((TimeSpan.FromHours(1))), now, signingCredentials);
+            var encodeJwt = handler.WriteToken(token);
+            return encodeJwt;
+        }
+    }
+}
---
+++ JWT.Bearer.WebApi.Test/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.Bearer.WebApi.Test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //配置api验证规则
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => {
+                        var serverSecret = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["JWT:ServerSecret"]));
+
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            IssuerSigningKey = serverSecret,
+                            ValidIssuer = Configuration["JWT:Issuer"],
+                            ValidAudience = Configuration["JWT:Audience"]
+                        };
+                    });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //开启验证
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
